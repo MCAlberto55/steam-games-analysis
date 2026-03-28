03a2666a0b49485484d46c6fa4d780e252904fb7
MCAlberto55 <alberto99emc@outlook.com>
2026-03-28 03:01:43 -0600

    add configuration file for routes and variables

---
+++ src/config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+ROOT = Path(__file__).resolve().parent.parent
+
+# Routes
+SRC_PATH = ROOT / "src"
+DATA_PATH = ROOT / "data"
+OUTPUT_PATH = ROOT / "outputs"
+
+# Variables
+RAW_DATA_PATH = "jypenpen54534/steam-games-dataset-2021-2025-65k"
