fb5726453759bcd9dd95b3b455975668a651faad
MCAlberto55 <alberto99emc@outlook.com>
2026-07-11 00:53:10 -0600

    add load featured data function

--- src/data/processed_data.py
+++ src/data/processed_data.py
@@ -1,7 +1,13 @@
 import pandas as pd
 from src.utils.config import VALIDATED_DATA_PATH
+from src.utils.config import FEATURE_ENGINEERED_DATA_PATH
 
 
 def get_validated_data():
     Validated_data = pd.read_parquet(VALIDATED_DATA_PATH)
     return Validated_data
+
+
+def get_featured_data():
+    Featured_data = pd.read_parquet(FEATURE_ENGINEERED_DATA_PATH, index_col=0)
+    return Featured_data
