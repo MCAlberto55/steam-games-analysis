58e2b3e7c453afcd4f93f96148051544f4837589
MCAlberto55 <alberto99emc@outlook.com>
2026-07-11 01:42:49 -0600

    add a function to return featured engineered data

--- src/data/processed_data.py
+++ src/data/processed_data.py
@@ -1,7 +1,13 @@
 import pandas as pd
 from src.utils.config import VALIDATED_DATA_PATH
+from src.utils.config import FEATURE_ENGINEERED_DATA_PATH
 
 
 def get_validated_data():
     Validated_data = pd.read_parquet(VALIDATED_DATA_PATH)
     return Validated_data
+
+
+def get_feature_engineered_data():
+    Feature_engineered_data = pd.read_parquet(FEATURE_ENGINEERED_DATA_PATH)
+    return Feature_engineered_data
